265d68db4f9a55a28a0ad10278c86d743c9171a9
Paul Serna-Torre <psernatorre@gmail.com>
2026-08-03 20:28:06 -0700

    feat: control on model order reduction

--- model_reduction_control/run.py
+++ model_reduction_control/run.py
@@ -6,6 +6,9 @@ from sting.modules.model_order_reduction.reductions import BalancedTruncation, S
 from sting.utils.dynamical_systems import smooth_step
 from pathlib import Path
 from wscc_9 import wscc_9
+from scipy.linalg import solve_continuous_are, eigvals, block_diag
+import cvxpy as cp
+from cmaspy.partial_state_feedback import single_agent_output_feedback, mas_output_feedback
 
 # Location of all outputs
 case_directory = os.path.join(Path(__file__).resolve().parent)
@@ -54,3 +57,38 @@ fom.simulate_ssm(t_max=t_max, inputs=inputs)
 rom.output_directory = os.path.join(case_directory, "outputs", "balanced_truncation_simulation")
 os.makedirs(rom.output_directory , exist_ok=True)
 rom.simulate_ssm(t_max=t_max, inputs=inputs)
+
+# Design of the output feedback control
+# Matrix of A of ROM
+A_c = rom.model.A
+
+# inputs = [p_ref, q_ref, v_ref, ...]
+B_c = rom.model.B[:, 0:3] # take only p_ref, q_ref, v_ref of the GFM in the proposed project
+
+# outputs = []
+C_c = np.zeros((10, A_c.shape[0]))
+C_c[0, 1] = 1 # w_pc
+C_c[1, 7] = 1 # i_vsc_d
+C_c[2, 8] = 1 # i_vsc_q
+C_c[3, 9] = 1 # i_bus_d
+C_c[4, 10] = 1 # i_bus_q 
+
+D_c = np.zeros((C_c.shape[0], B_c.shape[1]))
+
+Q = 10**5*np.eye(A_c.shape[0])
+
+R = 10**3*np.eye(B_c.shape[1])
+
+solve_settings = {'solver': cp.MOSEK,
+                  'verbose': False}
+
+# Solve CARE to obtain P
+P = solve_continuous_are(A_c, B_c, Q, R)
+
+# Use MAS output feedback
+alpha_coef = 1000
+beta_coef = 0
+gamma_coef = 0
+mas_out = mas_output_feedback(A_c, [B_c], [C_c], [D_c], [Q], [R], [P], alpha_coef, beta_coef, gamma_coef, **solve_settings)
+
+print(mas_out.F[0])
