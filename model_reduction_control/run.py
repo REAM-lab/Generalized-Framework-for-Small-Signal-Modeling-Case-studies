8d912c8e69b9e8628fcea50f7339b1e43b7224cf
Paul Serna-Torre <psernatorre@gmail.com>
2026-08-11 18:50:47 -0700

    feat: model order reduction and control working with controller

--- model_reduction_control/run.py
+++ model_reduction_control/run.py
@@ -6,6 +6,7 @@ from sting.modules.model_order_reduction.reductions import BalancedTruncation, S
 from sting.utils.dynamical_systems import smooth_step
 from pathlib import Path
 from wscc_9 import wscc_9
+from wscc_9_controller import wscc_9_with_controller
 from scipy.linalg import solve_continuous_are, eigvals, block_diag
 import cvxpy as cp
 from cmaspy.partial_state_feedback import single_agent_output_feedback, mas_output_feedback
@@ -22,6 +23,17 @@ system.apply("post_system_init", system)
 # Construct a small-signal model
 system, fom = main.run_ssm(system=system, case_directory=case_directory)
 
+inputs = {
+    'gfmi_18a_0': {
+        'v_ref': lambda t: smooth_step(t, step_time=0.1, initial_value=0.0, final_value=0.10, transient_width=5e-3),
+        }
+}
+
+# Run SSM simulation
+fom.simulate_ssm(t_max=2.0, inputs=inputs)
+
+# Run EMT simulation
+#main.run_emt(inputs=inputs, t_max=2.0, system=system, case_directory=case_directory)
 
 # Create a reduced order model of all components in the zone labeled as "external".
 # We will then connect this reduced order model to the zone labeled "study", which
@@ -66,7 +78,7 @@ A_c = rom.model.A
 B_c = rom.model.B[:, 0:3] # take only p_ref, q_ref, v_ref of the GFM in the proposed project
 
 # outputs = []
-C_c = np.zeros((10, A_c.shape[0]))
+C_c = np.zeros((5, A_c.shape[0]))
 C_c[0, 1] = 1 # w_pc
 C_c[1, 7] = 1 # i_vsc_d
 C_c[2, 8] = 1 # i_vsc_q
@@ -92,3 +104,13 @@ gamma_coef = 0
 mas_out = mas_output_feedback(A_c, [B_c], [C_c], [D_c], [Q], [R], [P], alpha_coef, beta_coef, gamma_coef, **solve_settings)
 
 print(mas_out.F[0])
+
+
+# Run EMT simulation
+system2 = wscc_9_with_controller(case_directory=case_directory)
+
+# Apply any post initialization "updates" of system components
+system2.apply("post_system_init", system2)
+
+# Run EMT simulation
+main.run_emt(inputs=inputs, t_max=2.0, system=system2, case_directory=case_directory)
